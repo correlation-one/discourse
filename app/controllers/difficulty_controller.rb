0fb71cb40883fd3352de6b8ae6e696f939745a8c
Avik Hasija <avikhasija@gmail.com>
2018-05-03 11:03:04 -0400

    set difficulty function

--- app/controllers/difficulty_controller.rb
+++ app/controllers/difficulty_controller.rb
@@ -2,3 +2,10 @@ class DifficultyController < ApplicationController
     def set_difficulty
         params.require(:topic_id)
         params.require(:difficulty)
+
+       topic = Topic.find(params[:topic_id].to_i)
+       topic.custom_fields["difficulty"] = params[:difficulty].to_i
+       topic.save_custom_fields(true)
+
+       #render json (??)
+    end
